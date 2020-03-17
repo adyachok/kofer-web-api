2b7a1ab97d345ea007a3298d102da93a30932166
Andras Gyacsok <andras.gyacsok@boehringer-ingelheim.com>
2020-03-17 12:56:04 +0100

    ModelTask asdict customization.

--- src/models/faust_dao.py
+++ src/models/faust_dao.py
@@ -2,7 +2,7 @@ from abc import ABC
 from enum import Enum
 
 import faust
-from typing import Any, Optional
+from typing import Any, Optional, List
 
 from src.utils.fields import ChoiceField
 
@@ -22,12 +22,29 @@ class State(Enum):
     ERROR = 'error'
 
 
-class ModelTask(faust.Record):
+class CalculationItem(faust.Record, serializer='json'):
+    name: str
+    type: str = ChoiceField(choices=['float', 'int', 'str', 'array', 'byte'],
+                            default='str')
+    value: Any
+
+
+class ModelTask(faust.Record, serializer='json'):
     _id: Optional[str]
     model_name: str
-    data: dict
+    data: List[CalculationItem]
     result: Optional[dict]
     # state: State = State.QUEUED
     state: str = ChoiceField(choices=['QUEUED', 'IN_PROGRESS', 'FINISHED',
                                       'ERROR'],
                              default='QUEUED', required=False)
+
+    def _prepare_dict(self, data):
+        # Remove keys with None values from payload.
+        prepared_dict = {}
+        for k, v in data.items():
+            if isinstance(v, list) and len(v):
+                if isinstance(v[0], CalculationItem):
+                    v = [item.asdict() for item in v]
+            prepared_dict[k] = v
+        return prepared_dict
